fd7e7ffc75f07b534542d53c096c1ca946f673f0
thegoodcode <rhapsody@tuta.io>
2021-10-02 18:36:07 +0100

    add colors library to colorize console logs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
 import connectDB from './config/db.js'
+import colors from 'colors'
 
 dotenv.config()
 connectDB()
@@ -21,5 +22,6 @@ app.listen(
 	PORT,
 	console.log(
 		`Server started running in ${process.env.NODE_ENV} mode on port ${PORT}`
+			.magenta.underline
 	)
 )
