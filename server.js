035688f73e4a1bad0df80eeb63e045a8b5735a41
thegoodcode <rhapsody@tuta.io>
2021-10-03 14:39:18 +0100

    add custom error handling via middlewares

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
-import colors from 'colors'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -12,6 +13,9 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(
 	PORT,
